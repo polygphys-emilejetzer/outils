da8adba165a97a2f469cccb58a81c91f72fb6b0a
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-11-07 10:49:35 -0500

    Corrections à courriel

--- reseau/courriel.py
+++ reseau/courriel.py
@@ -198,7 +198,8 @@ class Courriel:
                 if pj.get('Content-Disposition', '').startswith('attachment'):
                     nom = pj.get_filename()
                     content_type = pj.get_content_type()
-                    contenu = pj.get_payload()
+                    pj.set_charset('utf-8')
+                    contenu = pj.get_payload(decode=True)
                     yield PièceJointe(nom, content_type, contenu)
 
     def envoyer(self, adresse, port=25):
